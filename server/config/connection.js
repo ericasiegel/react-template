211eed932de943431e7b62a6e2a95bec3a3511fa
Erica Siegel <siegel.erica@gmail.com>
2021-07-05 10:40:29 -1000

    added apollo connections to server.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/the-cq-system', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/project-name', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
